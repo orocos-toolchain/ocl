f2c06c19ba71590fcbbe3fd8a264dd17391ae900
rsmits <rsmits@ce417995-dfc9-0310-95a0-acdaff106893>
2006-06-13 11:32:12 +0000

    added reporter to kuka160-test
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-components@4801 ce417995-dfc9-0310-95a0-acdaff106893

--- hardware/kuka/main.cpp
+++ hardware/kuka/main.cpp
@@ -1,11 +1,13 @@
-// hardware interfaces
+//hardware interfaces
 
 #include "Kuka160nAxesVelocityController.hpp"
 
 //User interface
 #include "../../taskbrowser/TaskBrowser.hpp"
 
-//main
+//Reporting
+#include "../../reporting/FileReporting.hpp"
+
 //#include <corelib/Activities.hpp>
 //#include <execution/GenericTaskContext.hpp>
 //#include <corelib/Logger.hpp>
@@ -19,38 +21,37 @@ class EmergencyStop
 {
 public:
   EmergencyStop(GenericTaskContext *axes)
-    : _axes(axes), _fired(false)  {
-    _stop = _axes->commands()->create("this", "stopAllAxes");
-    _lock = _axes->commands()->create("this", "lockAllAxes");
+    : _axes(axes) {
+    _stop = _axes->commands()->create("this", "stopAxis").arg(_axis).arg(_value);
+    _lock = _axes->commands()->create("this", "lockAxis").arg(_axis).arg(_value);
   };
   ~EmergencyStop(){};
-  void Handle() {
-    if (!_fired){
-	_stop.execute();
-	_lock.execute();
-    }
-  };
-  void Complete(){
-    if (!_fired){ _fired = true;
+  void callback(int axis, double value) {
+    _axis = axis;
+    _value = value;
+    _stop.execute();
+    _lock.execute();
     cout << "---------------------------------------------" << endl;
     cout << "--------- EMERGENCY STOP --------------------" << endl;
-    cout << "---------------------------------------------" << endl; }};
+    cout << "---------------------------------------------" << endl;
+    cout << "Axis "<< _axis <<" drive value "<<_value<< " reached limitDriveValue"<<endl;
+  };
 private:
   GenericTaskContext *_axes;
-  bool _fired;
   CommandC _stop;
   CommandC _lock;
+  int _axis;
+  double _value;
 }; // class
 
+void PositionLimitCallBack(int axis, double value)
+{
+  cout<< "-------------Warning----------------"<<endl;
+  cout<< "Axis "<<axis<<" moving passed software position limit, current value: "<<value<<endl;
+}
 
-using namespace Orocos;
-using namespace RTT;
-using namespace std;
+/// main() function
 
-
-/**
- * main() function
- */
 int ORO_main(int argc, char* argv[])
 {
 
@@ -60,21 +61,32 @@ int ORO_main(int argc, char* argv[])
   }
   
   RTT::GenericTaskContext* my_robot = new Kuka160nAxesVelocityController();
+
+  EmergencyStop _emergency(my_robot);
   
-  NonPreemptibleActivity _kukaTask(1, my_robot->engine() );  // very slow for debugging purposes
+  /// Creating Event Handlers
+  Handle _emergencyHandle = my_robot->events()->setupConnection("driveOutOfRange").
+    callback(&_emergency,&EmergencyStop::callback).handle();
+  Handle _positionWarning = my_robot->events()->setupConnection("positionOutOfRange").
+    callback(&PositionLimitCallBack).handle();
+
+  /// Connecting Event Handlers
+  _emergencyHandle.connect();
+  _positionWarning.connect();
   
+  /// Link my_robot to Taskbrowser
   TaskBrowser browser( my_robot );
-  
   browser.setColorTheme( TaskBrowser::whitebg );
   
-  /**
-   * Start the console reader.
-   */
-  browser.loop();
+  ///Reporting
+  FileReporting reporter("Reporting");
+  reporter.connectPeers(my_robot);
   
-  /**
-   * End of C++ examples, start TaskBrowser to let user play:
-   */
+  /// Creating Task
+  NonPreemptibleActivity _kukaTask(1, my_robot->engine() );  // very slow for debugging purposes
+  PeriodicActivity reportingTask(10,1.0,reporter.engine());
+  /// Start the console reader.
+  browser.loop();
   
   _kukaTask.stop();
 
