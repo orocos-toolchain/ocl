orocos-toolchain/ocl

commit 6f755ea25b8e1fa055d175ae3709efd51a1ebe94

Improve implementation by using 'DeviceInfo' struct which allows to hide comedi includes fully in the cpp files.
git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-components@5752 ce417995-dfc9-0310-95a0-acdaff106893

hardware/comedi/ComediEncoder.cpp
@@ -41,7 +41,7 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 //  
 
-#include "rtt/dev/ComediEncoder.hpp"
+#include "ComediEncoder.hpp"
 #include <rtt/os/fosi.h>
 
 #include "comedi_internal.h"

hardware/comedi/ComediDevice.cpp
@@ -45,7 +45,7 @@
    Major rewrite of original file by Peter Soetens
 */
 
-#include "rtt/dev/ComediDevice.hpp"
+#include "ComediDevice.hpp"
 #include <rtt/os/fosi.h>
 
 #include "comedi_internal.h"
@@ -60,29 +60,15 @@ namespace RTT
   typedef unsigned int Data;
 
   ComediDevice::ComediDevice( unsigned int minor )
-    : devminor( minor ), error( 0 )
+      : d( new DeviceInfo( minor ) )
   {
-    it = 0;
-
-    char devString[ 15 ];
-    sprintf( devString, "/dev/comedi%d", devminor );
-
-    it = comedi_open( devString );
-
-    rtos_printf( "Trying to open %s\n", devString );
-
-    if ( it == 0 )
-      {
-	rtos_printf( "comedi_open failed\n" );
-	error = -EINVAL;
-      }
   }
 
   ComediDevice::~ComediDevice()
   {
-    if (comedi_close(it) < 0)
+    if (comedi_close(d->it) < 0)
       {
-	rtos_printf( "comedi_close failed\n" );
+          rtos_printf( "comedi_close failed\n" );
       }
   }
 
@@ -91,12 +77,12 @@ namespace RTT
   Data ComediDevice::getMaxData(unsigned int subd)
   {
     unsigned int channel = 0;
-    return (Data) comedi_get_maxdata( it, subd, channel );
+    return (Data) comedi_get_maxdata( d->it, subd, channel );
   }
 
   int ComediDevice::getSubDeviceType(unsigned int subd)
   {
-    return comedi_get_subdevice_type( it, subd );
+    return comedi_get_subdevice_type( d->it, subd );
   }
 
   int ComediDevice::read( unsigned int subd, unsigned int chanNr,
@@ -104,10 +90,10 @@ namespace RTT
 			  ComediDevice::Data& value )
   {
     value = 0;
-    if ( error )
+    if ( d->error )
       return -1;
 
-    comedi_data_read( it, subd, chanNr, range, aref, 
+    comedi_data_read( d->it, subd, chanNr, range, aref, 
 		      ( lsampl_t* ) & value );
 
     return 0;
@@ -117,19 +103,19 @@ namespace RTT
 			   unsigned int range, unsigned int aref,
 			   const ComediDevice::Data& value)
   {
-    if ( error )
+    if ( d->error )
       return -1;
 
     Data output = value; 
 
-    comedi_data_write( it, subd, chanNr, range, aref, output );
+    comedi_data_write( d->it, subd, chanNr, range, aref, output );
 
     return 0;
   }
 
-  comedi_t* ComediDevice::getDevice() 
-  { 
-    return it; 
-  }
+    ComediDevice::DeviceInfo* ComediDevice::getDevice() 
+    { 
+        return d.get(); 
+    }
 
 }

hardware/comedi/ComediDevice.hpp
@@ -22,21 +22,18 @@
 #ifndef COMEDIDEVICE_HPP
 #define COMEDIDEVICE_HPP
 
-#include <pkgconf/system.h>
-#ifdef OROPKG_OS_LXRT
-typedef void comedi_t;
-#else
-typedef struct comedi_t_struct comedi_t;
-#endif
+#include <boost/shared_ptr.hpp>
 
 namespace RTT
 {
-
   /**
    * Some small class for using comedi in C++.
    */
   class ComediDevice
   {
+  protected:
+      class DeviceInfo;
+      boost::shared_ptr<DeviceInfo> d;
   public:
     typedef unsigned int Data;
 
@@ -110,17 +107,10 @@ namespace RTT
     int getSubDeviceType(unsigned int subd);
 
     /**
-       Return a pointer to a comedi_t struct
-       @todo check if this shouldn't be a protected method
+       Return a pointer to a comedi device info.
     */
-    comedi_t* getDevice();
+    DeviceInfo* getDevice();
         
-  protected:
-    unsigned int devminor;
-    int error;
-
-    comedi_t *it;
-
   };
 
 

hardware/comedi/ComediEncoder.hpp
@@ -20,10 +20,8 @@
 #define __COMEDI_ENCODER_INCREMENTAL_HPP
 
 
-#include <pkgconf/device_drivers_comedi.h>
-
-#include "SensorInterface.hpp"
-#include "EncoderInterface.hpp"
+#include "rtt/dev/SensorInterface.hpp"
+#include "rtt/dev/EncoderInterface.hpp"
 #include "ComediDevice.hpp"
 #include <string>
 

hardware/comedi/ComediSubDeviceAIn.cpp
@@ -27,7 +27,7 @@
  
  
 
-#include "rtt/dev/ComediSubDeviceAIn.hpp"
+#include "ComediSubDeviceAIn.hpp"
 #include "comedi_internal.h"
 #include <rtt/os/fosi.h>
 
@@ -64,7 +64,7 @@ namespace RTT
 	  rtos_printf( "comedi_get_subdevice_type failed\n" );
 	}
 
-      channels = comedi_get_n_channels(myCard->getDevice(), _subDevice);
+      channels = comedi_get_n_channels(myCard->getDevice()->it, _subDevice);
 
       _sd_range = new unsigned int[channels];
       _aref = new unsigned int[channels];
@@ -125,19 +125,19 @@ namespace RTT
 #ifdef __KERNEL__
       // See file:/usr/src/comedilib/doc/html/x3563.html#REF-TYPE-COMEDI-KRANGE
       comedi_krange range;
-      comedi_get_krange(myCard->getDevice(), _subDevice, chan, 
+      comedi_get_krange(myCard->getDevice()->it, _subDevice, chan, 
 			_sd_range[chan], &range);
       return (double) range.min / 1000000;
 #else
 #ifdef OROPKG_OS_LXRT
 //#define __KERNEL__
       comedi_krange range;
-      comedi_get_krange(myCard->getDevice(), _subDevice, chan, 
+      comedi_get_krange(myCard->getDevice()->it, _subDevice, chan, 
 			_sd_range[chan], &range);
       return (double) range.min / 1000000;
 #else // Userspace
       comedi_range * range_p;
-      if ((range_p = comedi_get_range(myCard->getDevice(), 
+      if ((range_p = comedi_get_range(myCard->getDevice()->it, 
 				      _subDevice, chan, 
 				      _sd_range[chan])) != 0)
 	{
@@ -161,19 +161,19 @@ namespace RTT
 #ifdef __KERNEL__
       // See file:/usr/src/comedilib/doc/html/x3563.html#REF-TYPE-COMEDI-KRANGE
       comedi_krange range;
-      comedi_get_krange(myCard->getDevice(), _subDevice, chan, 
+      comedi_get_krange(myCard->getDevice()->it, _subDevice, chan, 
 			_sd_range[chan], &range);
       return (double) range.max / 1000000;
 #else
 #ifdef OROPKG_OS_LXRT
 //#define __KERNEL__
       comedi_krange range;
-      comedi_get_krange(myCard->getDevice(), _subDevice, chan, 
+      comedi_get_krange(myCard->getDevice()->it, _subDevice, chan, 
 			_sd_range[chan], &range);
       return (double) range.max / 1000000;
 #else // Userspace
       comedi_range * range_p;
-      if ((range_p = comedi_get_range(myCard->getDevice(), 
+      if ((range_p = comedi_get_range(myCard->getDevice()->it, 
 				      _subDevice, chan, 
 				      _sd_range[chan])) != 0)
 	{

hardware/comedi/ComediSubDeviceAIn.hpp
@@ -34,7 +34,7 @@
 #ifndef COMEDISUBDEVICEAIN_HPP
 #define COMEDISUBDEVICEAIN_HPP
 
-#include "AnalogInInterface.hpp"
+#include "rtt/dev/AnalogInInterface.hpp"
 #include "ComediDevice.hpp"
 
 namespace RTT

hardware/comedi/ComediSubDeviceAOut.cpp
@@ -28,8 +28,8 @@
    stuff.
 */
 
-#include "rtt/dev/ComediSubDeviceAOut.hpp"
-#include "rtt/dev/ComediDevice.hpp"
+#include "ComediSubDeviceAOut.hpp"
+#include "ComediDevice.hpp"
 #include <rtt/os/fosi.h>
 #include "comedi_internal.h"
 
@@ -68,7 +68,7 @@ namespace RTT
 	  rtos_printf( "comedi_get_subdevice_type failed\n" );
 	}
 
-      channels = comedi_get_n_channels(myCard->getDevice(), _subDevice);
+      channels = comedi_get_n_channels(myCard->getDevice()->it, _subDevice);
 
       _sd_range = new unsigned int[channels];
       _aref = new unsigned int[channels];
@@ -129,19 +129,19 @@ namespace RTT
 #ifdef __KERNEL__
       // See file:/usr/src/comedilib/doc/html/x3563.html#REF-TYPE-COMEDI-KRANGE
       comedi_krange range;
-      comedi_get_krange(myCard->getDevice(), _subDevice, chan, 
+      comedi_get_krange(myCard->getDevice()->it, _subDevice, chan, 
 			_sd_range[chan], &range);
       return (double) range.min / 1000000;
 #else
 #ifdef OROPKG_OS_LXRT
 //#define __KERNEL__
       comedi_krange range;
-      comedi_get_krange(myCard->getDevice(), _subDevice, chan, 
+      comedi_get_krange(myCard->getDevice()->it, _subDevice, chan, 
 			_sd_range[chan], &range);
       return (double) range.min / 1000000;
 #else // Userspace
       comedi_range * range_p;
-      if ((range_p = comedi_get_range(myCard->getDevice(), 
+      if ((range_p = comedi_get_range(myCard->getDevice()->it, 
 				      _subDevice, chan, 
 				      _sd_range[chan])) != 0)
 	{
@@ -165,20 +165,20 @@ namespace RTT
 #ifdef __KERNEL__
       // See file:/usr/src/comedilib/doc/html/x3563.html#REF-TYPE-COMEDI-KRANGE
       comedi_krange range;
-      comedi_get_krange(myCard->getDevice(), _subDevice, chan, 
+      comedi_get_krange(myCard->getDevice()->it, _subDevice, chan, 
 			_sd_range[chan], &range);
       return (double) range.max / 1000000;
 #else
 #ifdef OROPKG_OS_LXRT
 //#define __KERNEL__
       comedi_krange range;
-      comedi_get_krange(myCard->getDevice(), _subDevice, chan, 
+      comedi_get_krange(myCard->getDevice()->it, _subDevice, chan, 
 			_sd_range[chan], &range);
       return (double) range.max / 1000000;
       
 #else // Userspace
       comedi_range * range_p;
-      if ((range_p = comedi_get_range(myCard->getDevice(), 
+      if ((range_p = comedi_get_range(myCard->getDevice()->it, 
 				      _subDevice, chan, 
 				      _sd_range[chan])) != 0)
 	{

hardware/comedi/ComediSubDeviceAOut.hpp
@@ -31,7 +31,7 @@
 #ifndef COMEDISUBDEVICEAOUT_HPP
 #define COMEDISUBDEVICEAOUT_HPP
 
-#include "AnalogOutInterface.hpp"
+#include "rtt/dev/AnalogOutInterface.hpp"
 #include "ComediDevice.hpp"
 
 namespace RTT

commit 83fc191e2674ca05a17bb6014aa83ed66c61dc0f

fix bug  #229, regression introduced by r5752
git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-components@5818 ce417995-dfc9-0310-95a0-acdaff106893

hardware/comedi/ComediEncoder.cpp
@@ -74,13 +74,13 @@ namespace RTT
                            << _myCard->getSubDeviceType(_subDevice) << endlog();
             }
         // Check how many counters this subdevice actually has
-        unsigned int nchan = comedi_get_n_channels(_myCard->getDevice(),_subDevice);
+        unsigned int nchan = comedi_get_n_channels(_myCard->getDevice()->it,_subDevice);
         if ( nchan <= _channel )
             {
                 log(Error) << "Comedi Counter : Only " << nchan 
                            << " channels on this counter subdevice" << endlog();
             }
-        _resolution = (int) comedi_get_maxdata(_myCard->getDevice(),_subDevice, 
+        _resolution = (int) comedi_get_maxdata(_myCard->getDevice()->it,_subDevice, 
                                                _channel);
         /* Configure the counter subdevice
            Configure the GPCT for use as an encoder 
@@ -103,7 +103,7 @@ namespace RTT
         insn.data=config_data;
         insn.subdev=_subDevice;
         insn.chanspec=CR_PACK(_channel,0,0);
-        int ret=comedi_do_insn(_myCard->getDevice(),&insn);
+        int ret=comedi_do_insn(_myCard->getDevice()->it,&insn);
         if(ret<0)
             log(Error) << "Comedi Counter : Instruction to configure counter -> encoder failed" << endlog();
         else
@@ -116,7 +116,7 @@ namespace RTT
     {
         //int can be negative, by casting the int to lsampl_t(unsigned int)
         // we write the right value to the encoderdevice     
-        comedi_data_write(_myCard->getDevice(), _subDevice,
+        comedi_data_write(_myCard->getDevice()->it, _subDevice,
                           _channel, 0, 0, (lsampl_t) p);
     }
 
@@ -128,7 +128,7 @@ namespace RTT
         typedef unsigned int Data;
         //int pos;
         lsampl_t pos[20];
-        int ret=comedi_data_read(_myCard->getDevice(),_subDevice,_channel,0,0,pos);
+        int ret=comedi_data_read(_myCard->getDevice()->it,_subDevice,_channel,0,0,pos);
         //int ret=comedi_data_read(_myCard->getDevice(),_subDevice,_channel,0,0,(unsigned int *)&pos);
         if(ret<0){
             log(Error) << "Comedi Counter : reading encoder failed, ret = " << ret << endlog();

hardware/comedi/ComediPulseTrainGenerator.cpp
@@ -63,7 +63,7 @@ namespace RTT
                        << _myCard->getSubDeviceType(_subDevice) << endlog();
         }
         // Check how many counters this subdevice actually has
-        unsigned int nchan = comedi_get_n_channels(_myCard->getDevice(),_subDevice);
+        unsigned int nchan = comedi_get_n_channels(_myCard->getDevice()->it,_subDevice);
         /* Set timebase to maximum, ie. 20 MHz for the 6601 and 80 MHz
            for the 6602 board
            FIXME:  Make this more flexible if comedi driver supports
@@ -100,7 +100,7 @@ namespace RTT
         insn.subdev=_subDevice;
         insn.chanspec=CR_PACK(_channel,0,0);
 
-        int ret=comedi_do_insn(_myCard->getDevice(),&insn);
+        int ret=comedi_do_insn(_myCard->getDevice()->it,&insn);
         if (ret<0)
             log(Error) << "Comedi Counter : Instruction to configure counter -> Pulse Train Generator failed, ret = " << ret << endlog();
         else
@@ -118,7 +118,7 @@ namespace RTT
         insn.data=inttrig_data;
         insn.subdev=_subDevice;
         insn.chanspec=CR_PACK(_channel,0,0);
-        ret=comedi_do_insn(_myCard->getDevice(),&insn);
+        ret=comedi_do_insn(_myCard->getDevice()->it,&insn);
         if(ret<0){
             log(Error) << "Triggering reset on ComediPTG () failed, ret = " << ret << endlog();
         }
@@ -141,7 +141,7 @@ namespace RTT
             insn.subdev=_subDevice;
             insn.chanspec=CR_PACK(_channel,0,0);
 
-            int ret=comedi_do_insn(_myCard->getDevice(),&insn);
+            int ret=comedi_do_insn(_myCard->getDevice()->it,&insn);
             if (ret<0){
                 log(Error) << "Comedi Counter : Changing the PTG period failed, ret = " << ret << endlog();
                 return false;
@@ -170,7 +170,7 @@ namespace RTT
             insn.subdev=_subDevice;
             insn.chanspec=CR_PACK(_channel,0,0);
 
-            int ret=comedi_do_insn(_myCard->getDevice(),&insn);
+            int ret=comedi_do_insn(_myCard->getDevice()->it,&insn);
             if (ret<0){
                 log(Error) << "Comedi Counter : Changing the PTG period failed, ret = " << ret << endlog();
                 return false;
@@ -197,7 +197,7 @@ namespace RTT
             insn.data=config_data;
             insn.subdev=_subDevice;
             insn.chanspec=CR_PACK(_channel,0,0);
-            ret = comedi_do_insn(_myCard->getDevice(),&insn);
+            ret = comedi_do_insn(_myCard->getDevice()->it,&insn);
             if(ret<0){
                 log(Error) << "ComediPTG::start() failed, ret = " << ret << endlog();
                 return false;
@@ -221,7 +221,7 @@ namespace RTT
             insn.data=config_data;
             insn.subdev=_subDevice;
             insn.chanspec=CR_PACK(_channel,0,0);
-            ret = comedi_do_insn(_myCard->getDevice(),&insn);
+            ret = comedi_do_insn(_myCard->getDevice()->it,&insn);
             if(ret<0){
                 log(Error) << "ComediPTG::start() failed, ret = " << ret << endlog();
                 return false;