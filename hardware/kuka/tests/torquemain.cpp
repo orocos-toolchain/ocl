3f388eed0e6821c485862ebd7f1162db38efaafa
rsmits <rsmits@ce417995-dfc9-0310-95a0-acdaff106893>
2006-10-27 13:05:51 +0000

    adding test for torquecontrol
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-components@6098 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ hardware/kuka/tests/torquemain.cpp
@@ -0,0 +1,54 @@
+//hardware interfaces
+
+#include <hardware/kuka/Kuka361nAxesTorqueController.hpp>
+#include <hardware/kuka/EmergencyStop.hpp>
+
+
+//User interface
+#include <taskbrowser/TaskBrowser.hpp>
+
+//Reporting
+#include <reporting/FileReporting.hpp>
+
+#include <rtt/os/main.h>
+
+using namespace Orocos;
+using namespace RTT;
+using namespace std;
+
+/// main() function
+
+int ORO_main(int argc, char* argv[])
+{
+  if ( Logger::log().getLogLevel() < Logger::Debug ) {
+    Logger::log().setLogLevel( Logger::Debug );
+    log(Info) << argv[0] << " manually raises LogLevel to 'Debug'."
+              << " See also file 'orocos.log'." << endlog();
+  }
+
+  TaskContext* my_robot = new Kuka361nAxesTorqueController("Kuka361");
+  
+  EmergencyStop _emergency(my_robot);
+  
+  /// Creating Event Handlers
+  _emergency.addEvent(my_robot,"positionOutOfRange");
+  _emergency.addEvent(my_robot,"velocityOutOfRange");
+  _emergency.addEvent(my_robot,"currentOutOfRange");
+  
+  /// Link my_robot to Taskbrowser
+  TaskBrowser browser(my_robot );
+  browser.setColorTheme( TaskBrowser::whitebg );
+
+  //Loading program in browser
+  my_robot->scripting()->loadPrograms("cpf/program.ops");
+
+  /// Creating Tasks
+  PeriodicActivity _kukaTask(0,0.01, my_robot->engine() );  
+  
+  /// Start the console reader.
+  _kukaTask.start();
+
+  browser.loop();
+  
+  return 0;
+}
