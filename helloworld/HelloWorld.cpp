ee4b86584cecab9c25c03c04c8496584623ad78f
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2006-11-02 12:46:20 +0000

    Update helloworld API docs.
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/orocos-components@6137 ce417995-dfc9-0310-95a0-acdaff106893

--- helloworld/HelloWorld.cpp
+++ helloworld/HelloWorld.cpp
@@ -1,5 +1,6 @@
 
 /**
+ * @file HelloWorld.cpp
  * This file demonstrate each Orocos primitive with
  * a 'hello world' example.
  */
@@ -17,184 +18,190 @@
 #include <rtt/Ports.hpp>
 #include <rtt/PeriodicActivity.hpp>
 
+#include <ocl/OCL.hpp>
+
 using namespace std;
 using namespace RTT;
 using namespace Orocos;
 
-/**
- * Every component inherits from the 'TaskContext' class.  This base
- * class allow a user to add a primitive to the interface and contain
- * an ExecutionEngine which executes application code.
- */
-class HelloWorld
-    : public TaskContext
+namespace OCL
 {
-protected:
-    /**
-     * @defgroup params Name-Value parameters:
-     * @{
-     */
-    /**
-     * Properties take a name, a value and a description
-     * and are suitable for XML.
-     */
-    Property<std::string> property;
-    /**
-     * Attributes take a name and contain changing values.
-     */
-    Attribute<std::string> attribute;
-    /**
-     * Constants take a name and contain a constant value.
-     */
-    Constant<std::string> constant;
-    /** @} */
 
     /**
-     * @defgroup ports Input-Output ports:
-     * @{
+     * Every component inherits from the 'TaskContext' class.  This base
+     * class allow a user to add a primitive to the interface and contain
+     * an ExecutionEngine which executes application code.
      */
-    /**
-     * DataPorts share data among readers and writers.
-     * A reader always reads the most recent data.
-     */
-    DataPort<std::string> dataport;
-    /**
-     * BufferPorts buffer data among readers and writers.
-     * A reader reads the data in a FIFO way.
-     */
-    BufferPort<std::string> bufferport;
-    /** @} */
+    class HelloWorld
+        : public TaskContext
+    {
+    protected:
+        /**
+         * @name Name-Value parameters
+         * @{
+         */
+        /**
+         * Properties take a name, a value and a description
+         * and are suitable for XML.
+         */
+        Property<std::string> property;
+        /**
+         * Attributes take a name and contain changing values.
+         */
+        Attribute<std::string> attribute;
+        /**
+         * Constants take a name and contain a constant value.
+         */
+        Constant<std::string> constant;
+        /** @} */
 
-    /**
-     * @defgroup methods Method:
-     * @{
-     */
-    /**
-     * Methods take a number of arguments and
-     * return a value. The are executed in the
-     * thread of the caller.
-     */
-    Method<std::string(void)> method;
+        /**
+         * @name Input-Output ports
+         * @{
+         */
+        /**
+         * DataPorts share data among readers and writers.
+         * A reader always reads the most recent data.
+         */
+        DataPort<std::string> dataport;
+        /**
+         * BufferPorts buffer data among readers and writers.
+         * A reader reads the data in a FIFO way.
+         */
+        BufferPort<std::string> bufferport;
+        /** @} */
 
-    /**
-     * The method function is executed by
-     * the method object:
-     */
-    std::string mymethod() {
-        return "Hello World";
-    }
-    /** @} */
+        /**
+         * @name Method
+         * @{
+         */
+        /**
+         * Methods take a number of arguments and
+         * return a value. The are executed in the
+         * thread of the caller.
+         */
+        Method<std::string(void)> method;
 
-    /**
-     * @defgroup comm Command:
-     * @{
-     */
-    /**
-     * Commands take a number of arguments and
-     * return true or false. They are asynchronous
-     * and executed in the thread of the receiver.
-     */
-    Command<bool(std::string)> command;
+        /**
+         * The method function is executed by
+         * the method object:
+         */
+        std::string mymethod() {
+            return "Hello World";
+        }
+        /** @} */
 
-    /**
-     * The command function executed by the receiver.
-     */
-    bool mycommand(std::string arg) {
-        log() << "Hello Command: "<< arg << endlog();
-        if ( arg == "World" )
+        /**
+         * @name Command
+         * @{
+         */
+        /**
+         * Commands take a number of arguments and
+         * return true or false. They are asynchronous
+         * and executed in the thread of the receiver.
+         */
+        Command<bool(std::string)> command;
+
+        /**
+         * The command function executed by the receiver.
+         */
+        bool mycommand(std::string arg) {
+            log() << "Hello Command: "<< arg << endlog();
+            if ( arg == "World" )
+                return true;
+            else
+                return false;
+        }
+
+        /**
+         * The completion condition checked by the sender.
+         */
+        bool mycomplete(std::string arg) {
+            log() << "Checking Command: "<< arg <<endlog();
             return true;
-        else
-            return false;
-    }
+        }
+        /** @} */
 
-    /**
-     * The completion condition checked by the sender.
-     */
-    bool mycomplete(std::string arg) {
-        log() << "Checking Command: "<< arg <<endlog();
-        return true;
-    }
-    /** @} */
+        /**
+         * @name Event
+         * @{
+         */
+        /**
+         * The event takes a payload which is distributed
+         * to anonymous receivers. Distribution can happen
+         * synchronous and asynchronous.
+         */
+        Event<void(std::string)> event;
 
-    /**
-     * @defgroup evnt Event:
-     * @{
-     */
-    /**
-     * The event takes a payload which is distributed
-     * to anonymous receivers. Distribution can happen
-     * synchronous and asynchronous.
-     */
-    Event<void(std::string)> event;
+        /**
+         * Stores the connection between 'event' and 'mycallback'.
+         */
+        Handle h;
 
-    /**
-     * Stores the connection between 'event' and 'mycallback'.
-     */
-    Handle h;
+        /**
+         * An event callback (or subscriber) function.
+         */
+        void mycallback( std::string data )
+        {
+            log() << "Receiving Event: " << data << endlog();
+        }
+        /** @} */
 
-    /**
-     * An event callback (or subscriber) function.
-     */
-    void mycallback( std::string data )
-    {
-        log() << "Receiving Event: " << data << endlog();
-    }
-    /** @} */
+    public:
+        /**
+         * This example sets the interface up in the Constructor
+         * of the component.
+         */
+        HelloWorld(std::string name)
+            : TaskContext(name),
+              // Name, description, value
+              property("the_property", "the_property Description", "Hello World"),
+              // Name, value
+              attribute("the_attribute", "Hello World"),
+              // Name, value
+              constant("the_constant", "Hello World"),
+              // Name, initial value
+              dataport("the_data_port","World"),
+              // Name, buffer size, initial value
+              bufferport("the_buffer_port",13, "World"),
+              // Name, function pointer, object
+              method("the_method", &HelloWorld::mymethod, this),
+              // Name, command function pointer, completion condition function pointer, object
+              command("the_command", &HelloWorld::mycommand, &HelloWorld::mycomplete, this),
+              // Name
+              event("the_event")
+        {
+            // Check if all initialisation was ok:
+            assert( property.ready() );
+            assert( attribute.ready() );
+            assert( constant.ready() );
+            assert( method.ready() );
+            assert( command.ready() );
+            assert( event.ready() );
 
-public:
-    /**
-     * This example sets the interface up in the Constructor
-     * of the component.
-     */
-    HelloWorld(std::string name)
-        : TaskContext(name),
-          // Name, description, value
-          property("the_property", "the_property Description", "Hello World"),
-          // Name, value
-          attribute("the_attribute", "Hello World"),
-          // Name, value
-          constant("the_constant", "Hello World"),
-          // Name, initial value
-          dataport("the_data_port","World"),
-          // Name, buffer size, initial value
-          bufferport("the_buffer_port",13, "World"),
-          // Name, function pointer, object
-          method("the_method", &HelloWorld::mymethod, this),
-          // Name, command function pointer, completion condition function pointer, object
-          command("the_command", &HelloWorld::mycommand, &HelloWorld::mycomplete, this),
-          // Name
-          event("the_event")
-    {
-        // Check if all initialisation was ok:
-        assert( property.ready() );
-        assert( attribute.ready() );
-        assert( constant.ready() );
-        assert( method.ready() );
-        assert( command.ready() );
-        assert( event.ready() );
+            // Now add it to the interface:
+            this->properties()->addProperty(&property);
 
-        // Now add it to the interface:
-        this->properties()->addProperty(&property);
+            this->attributes()->addAttribute(&attribute);
+            this->attributes()->addConstant(&constant);
 
-        this->attributes()->addAttribute(&attribute);
-        this->attributes()->addConstant(&constant);
+            this->ports()->addPort(&dataport);
+            this->ports()->addPort(&bufferport);
 
-        this->ports()->addPort(&dataport);
-        this->ports()->addPort(&bufferport);
-
-        this->methods()->addMethod(&method, "'the_method' Description");
+            this->methods()->addMethod(&method, "'the_method' Description");
         
-        this->commands()->addCommand(&command, "'the_command' Description",
-                                     "the_arg", "Use 'World' as argument to make the command succeed.");
+            this->commands()->addCommand(&command, "'the_command' Description",
+                                         "the_arg", "Use 'World' as argument to make the command succeed.");
 
-        this->events()->addEvent(&event, "'the_event' Description",
-                                 "the_data", "The data of this event.");
+            this->events()->addEvent(&event, "'the_event' Description",
+                                     "the_data", "The data of this event.");
 
-        // Adding an asynchronous callback:
-        h = this->events()->setupConnection("the_event").callback(this, &HelloWorld::mycallback, this->engine()->events() ).handle();
-        h.connect();
-    }
-};
+            // Adding an asynchronous callback:
+            h = this->events()->setupConnection("the_event").callback(this, &HelloWorld::mycallback, this->engine()->events() ).handle();
+            h.connect();
+        }
+    };
+}
 
 int ORO_main(int argc, char** argv)
 {
