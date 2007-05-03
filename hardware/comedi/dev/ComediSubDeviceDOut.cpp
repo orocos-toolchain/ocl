3d5960b08cab5a1d7c83f43e47323609ff71b5c5
kgadeyne <kgadeyne@ce417995-dfc9-0310-95a0-acdaff106893>
2007-05-03 12:09:58 +0000

    fix some licenses of which I am (partly) the copyrightholder
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/ocl@27979 ce417995-dfc9-0310-95a0-acdaff106893

--- hardware/comedi/dev/ComediSubDeviceDOut.cpp
+++ hardware/comedi/dev/ComediSubDeviceDOut.cpp
@@ -1,13 +1,6 @@
-/***************************************************************************
-  tag: Peter Soetens  Wed Jan 18 14:11:40 CET 2006  ComediSubDeviceDOut.cxx 
+// Copyright (C) 2003 Klaas Gadeyne
 
-                        ComediSubDeviceDOut.cxx -  description
-                           -------------------
-    begin                : Wed January 18 2006
-    copyright            : (C) 2006 Peter Soetens
-    email                : peter.soetens@mech.kuleuven.be
- 
- ***************************************************************************
+/***************************************************************************
  *   This library is free software; you can redistribute it and/or         *
  *   modify it under the terms of the GNU Lesser General Public            *
  *   License as published by the Free Software Foundation; either          *
@@ -24,8 +17,6 @@
  *   Suite 330, Boston, MA  02111-1307  USA                                *
  *                                                                         *
  ***************************************************************************/
- 
- 
 
 #include "ComediSubDeviceDOut.hpp"
 #include "rtt/os/fosi.h"
