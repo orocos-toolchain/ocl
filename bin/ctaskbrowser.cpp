ad61c84d2fc37477a6754c70d1d535333989a667
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2007-05-03 13:53:59 +0000

    Deployer application and ctaskbrowser extended/tested with CORBA.
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/ocl@27992 ce417995-dfc9-0310-95a0-acdaff106893

--- bin/ctaskbrowser.cpp
+++ bin/ctaskbrowser.cpp
@@ -1,28 +1,43 @@
 
 #include <rtt/os/main.h>
 #include <rtt/corba/ControlTaskProxy.hpp>
+#include <rtt/corba/ControlTaskServer.hpp>
 #include <taskbrowser/TaskBrowser.hpp>
 #include <iostream>
 #include <string>
 
+using namespace RTT;
+using namespace RTT::Corba;
+
 int ORO_main(int argc, char** argv)
 {
     if ( argc == 1) {
-        cerr << "Please specify the CORBA ControlTask name or IOR to connect to." <<endl;
-        cerr << "  " << argv[0] << " [ComponentName | IOR]" <<endl;
+        std::cerr << "Please specify the CORBA ControlTask name or IOR to connect to." << std::endl;
+        std::cerr << "  " << argv[0] << " [ComponentName | IOR]" << std::endl;
         return -1;
     }
     std::string name = argv[1];
 
+    ControlTaskServer::InitOrb( argc, argv);
+
+    ControlTaskServer::ThreadOrb();
+
+    RTT::TaskContext* proxy;
     if ( name.substr(0, 4) == "IOR:" ) {
-        RTT::Corba::ControlTaskProxy proxy( name );
-        OCL::TaskBrowser tb( &proxy );
-        tb.loop();
+        proxy = RTT::Corba::ControlTaskProxy::Create( name, true );
     } else {
-        RTT::Corba::ControlTaskProxy proxy( name, true ); // is_ior = true
-        OCL::TaskBrowser tb( &proxy );
-        tb.loop();
+        proxy = RTT::Corba::ControlTaskProxy::Create( name ); // is_ior = true
     }
+
+    if (proxy == 0){
+        std::cerr << "CORBA system error while looking up " << name << std::endl;
+        return -1;
+    }
+    OCL::TaskBrowser tb( proxy );
+    tb.loop();
+
+    ControlTaskServer::ShutdownOrb();
+    ControlTaskServer::DestroyOrb();
 
     return 0;
 }
--- bin/deployer.cpp
+++ bin/deployer.cpp
@@ -1,13 +1,43 @@
 #include <rtt/os/main.h>
+#include <rtt/RTT.hpp>
 #include <taskbrowser/TaskBrowser.hpp>
 #include <deployment/DeploymentComponent.hpp>
 
-int ORO_main(int, char**)
+#ifdef OROPKG_CORBA
+#include <rtt/corba/ControlTaskServer.hpp>
+#endif
+
+int ORO_main(int argc, char** argv)
 {
-    OCL::DeploymentComponent dc("Deployer");
+    // deployer [DeployerName] [ -- TAO options ]
+    char* name = "Deployer";
+    if ( argc > 1 ) {
+        std::string arg = argv[1];
+        if ( arg != "--" )
+            name = argv[1];
+    }
+    OCL::DeploymentComponent dc( name );
     OCL::TaskBrowser tb( &dc );
+
+    /**
+     * If CORBA is enabled, export the DeploymentComponent
+     * as CORBA server.
+     */
+#ifdef OROPKG_CORBA
+    using namespace RTT::Corba;
+    ControlTaskServer::InitOrb( argc, argv );
+
+    ControlTaskServer::Create( &dc );
+
+    ControlTaskServer::ThreadOrb();
+#endif
 
     tb.loop();
 
+#ifdef OROPKG_CORBA
+    ControlTaskServer::ShutdownOrb();
+
+    ControlTaskServer::DestroyOrb();
+#endif
     return 0;
 }
