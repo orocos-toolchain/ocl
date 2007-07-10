9297d70c2fc7f08aceb385fd3199f1346098dcdf
rsmits <rsmits@ce417995-dfc9-0310-95a0-acdaff106893>
2007-07-10 12:53:39 +0000

    Added component to simulate various naxes machines
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/ocl@28392 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ hardware/robotsimulation/nAxesVelocityController.cpp
@@ -0,0 +1,128 @@
+// Copyright (C) 2007 Ruben Smits <ruben dot smits at mech dot kuleuven dot be>
+//  
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//  
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//  
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//  
+
+#include "nAxesVelocityController.hpp"
+
+#include <rtt/Logger.hpp>
+
+namespace OCL
+{
+    using namespace RTT;
+    using namespace std;
+    
+    nAxesVelocityController::nAxesVelocityController(string name, unsigned int _naxes, string _propertyfile)
+        : TaskContext(name,PreOperational),
+          propertyfile(_propertyfile),
+          naxes(_naxes),
+          driveValues(naxes,0),
+          positionValues(naxes,0),
+          simulation_axes(naxes),
+          M_startAxes("startAxes",&nAxesVelocityController::startAxes,this),
+          M_stopAxes("stopAxes",&nAxesVelocityController::stopAxes,this),
+          M_unlockAxes("unlockAxes",&nAxesVelocityController::unlockAxes,this),
+          M_lockAxes("lockAxes",&nAxesVelocityController::lockAxes,this),
+          D_driveValues("nAxesOutputVelocity",driveValues),
+          D_positionValues("nAxesSensorPosition",positionValues),
+          P_initialPositions("initialPositions","initial positions (rad) for the axes",positionValues)
+    {
+        methods()->addMethod(&M_startAxes,"start the simulation axes");
+        methods()->addMethod(&M_stopAxes,"stop the simulation axes");
+        methods()->addMethod(&M_lockAxes,"lock the simulation axes");
+        methods()->addMethod(&M_unlockAxes,"unlock the simulation axes");
+        
+        ports()->addPort(&D_driveValues);
+        ports()->addPort(&D_positionValues);
+        
+        properties()->addProperty(&P_initialPositions);
+    }
+    
+    bool nAxesVelocityController::configureHook()
+    {
+        this->marshalling()->readProperties(propertyfile);
+        
+        for (unsigned int i = 0; i <naxes; i++){
+            simulation_axes[i] = new SimulationAxis(P_initialPositions.value()[i]);
+        }
+        return true;
+    }
+
+    bool nAxesVelocityController::startHook()
+    {
+        return true;
+    }
+    
+    void nAxesVelocityController::updateHook()
+    {
+        D_driveValues.Get(driveValues);
+        for (unsigned int i=0;i<naxes;i++){
+            positionValues[i]=simulation_axes[i]->getSensor("Position")->readSensor();
+            if(simulation_axes[i]->isDriven())
+                simulation_axes[i]->drive(driveValues[i]);
+        }
+        D_positionValues.Set(positionValues);
+    }
+    
+    void nAxesVelocityController::cleanupHook()
+    {
+        simulation_axes.clear();
+    }
+    
+    void nAxesVelocityController::stopHook()
+    {
+    }
+    
+    bool nAxesVelocityController::startAxes()
+    {
+        bool retval=true;
+        for(vector<SimulationAxis*>::iterator axis=simulation_axes.begin();
+            axis!=simulation_axes.end();axis++)
+            retval&=(*axis)->drive(0.0);
+        return retval;
+    }
+
+    bool nAxesVelocityController::stopAxes()
+    {
+            bool retval=true;
+        for(vector<SimulationAxis*>::iterator axis=simulation_axes.begin();
+            axis!=simulation_axes.end();axis++)
+            retval&=(*axis)->stop();
+        return retval;
+    }
+
+    bool nAxesVelocityController::lockAxes()
+    {
+    bool retval=true;
+        for(vector<SimulationAxis*>::iterator axis=simulation_axes.begin();
+            axis!=simulation_axes.end();axis++)
+            retval&=(*axis)->lock();
+        return retval;
+        
+    }
+
+    bool nAxesVelocityController::unlockAxes()
+    {
+    bool retval=true;
+        for(vector<SimulationAxis*>::iterator axis=simulation_axes.begin();
+            axis!=simulation_axes.end();axis++)
+            retval&=(*axis)->unlock();
+        return retval;
+    
+    }
+}
+
+        
+          
---
+++ hardware/robotsimulation/nAxesVelocityController.hpp
@@ -0,0 +1,82 @@
+// Copyright (C) 2007 Ruben Smits <ruben dot smits at mech dot kuleuven dot be>
+//  
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//  
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//  
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+//  
+
+#ifndef SIMULATION_NAXES_VELOCITY_CONTROLLER_HPP
+#define SIMULATION_NAXES_VELOCITY_CONTROLLER_HPP
+
+#include <rtt/RTT.hpp>
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Ports.hpp>
+#include <rtt/Event.hpp>
+#include <rtt/Method.hpp>
+#include <rtt/Properties.hpp>
+
+#include "dev/SimulationAxis.hpp"
+#include <rtt/dev/AxisInterface.hpp>
+
+#include <ocl/OCL.hpp>
+
+namespace OCL
+{
+    /**
+     * This class implements a Taskcontext that simulates a robot with
+     * a configurable number of axes.
+     * 
+     */
+
+    class nAxesVelocityController: public RTT::TaskContext
+    {
+    public:
+        nAxesVelocityController(std::string name,unsigned int nrofjoints, std::string propertyfile="cpf/nAxesVelocityController.cpf");
+        virtual ~nAxesVelocityController();
+    
+    private:
+        virtual bool startAxes();
+        virtual bool stopAxes();
+        virtual bool lockAxes();
+        virtual bool unlockAxes();
+        
+        virtual bool configureHook();
+        virtual bool startHook();
+        virtual void updateHook();
+        virtual void cleanupHook();
+        virtual void stopHook();
+        
+        const std::string propertyfile;
+        
+        unsigned int naxes;
+        
+        std::vector<double> driveValues;
+        std::vector<double> positionValues;    
+        
+        std::vector<RTT::SimulationAxis*> simulation_axes;
+
+    protected:
+        Method<bool(void)> M_startAxes;
+        Method<bool(void)> M_stopAxes;
+        Method<bool(void)> M_unlockAxes;
+        Method<bool(void)> M_lockAxes; 
+        
+        RTT::DataPort<std::vector<double> > D_driveValues;
+        RTT::DataPort<std::vector<double> > D_positionValues;
+        
+        RTT::Property<std::vector<double> > P_initialPositions;
+    
+    };
+}
+#endif
