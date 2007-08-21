91ab2e7d67e5550c5814c0ba2ce11273adae40c9
rsmits <rsmits@ce417995-dfc9-0310-95a0-acdaff106893>
2007-08-21 13:49:39 +0000

    removing seperated effector and sensor and combined them into one component supplying a cartesian position and receiving a cartesian velocity
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/ocl@28531 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ motion_control/cartesian/CartesianVelocityController.cpp
@@ -0,0 +1,117 @@
+// Copyright  (C)  2007  Ruben Smits <ruben dot smits at mech dot kuleuven dot be>
+
+// Author: Ruben Smits <ruben dot smits at mech dot kuleuven dot be>
+// URL: http://www.orocos.org/ocl
+
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+#include "CartesianVelocityController.hpp"
+
+namespace OCL
+{
+    using namespace std;
+    using namespace RTT;
+    using namespace KDL;
+
+    CartesianVelocityController::CartesianVelocityController(string name,const KDL::Chain& _chain, 
+                                                             KDL::ChainIkSolverVel* _iksolver,
+                                                             KDL::ChainFkSolverPos* _fksolver) :
+        TaskContext(name),
+        chain(_chain),
+        fksolver(_fksolver),
+        iksolver(_iksolver),
+        nj(chain.getNrOfJoints()),
+        jointpositions(nj),
+        jointvelocities(nj),
+        naxesposition(nj),
+        naxesvelocities(nj),
+        cartpos_port("CartesianSensorPosition",KDL::Frame::Identity()),
+        cartvel_port("CartesianOutputVelocity",KDL::Twist::Zero()),
+        naxespos_port("nAxesSensorPosition",std::vector<double>(nj,0)),
+        naxesvel_port("nAxesOutputVelocity",std::vector<double>(nj,0)),
+        kinematics_status(true),own_fk(false),own_ik(false)
+    {
+        //Create the ports
+        this->ports()->addPort(&cartpos_port);
+        this->ports()->addPort(&cartvel_port);
+        this->ports()->addPort(&naxespos_port);
+        this->ports()->addPort(&naxesvel_port);
+        
+        //If no solvers are given create our own:
+        if(iksolver==NULL){
+            iksolver=new ChainIkSolverVel_pinv(chain);
+            own_ik=true;
+        }
+        if(fksolver==NULL){
+            fksolver=new ChainFkSolverPos_recursive(chain);
+            own_fk=true;
+        }
+        
+    }
+
+    CartesianVelocityController::~CartesianVelocityController()
+    {
+        if (own_fk)
+            delete fksolver;
+        if (own_ik)
+            delete iksolver;
+    }
+    
+
+    bool CartesianVelocityController::startHook()
+    {
+        //Initialize: calculate everything once
+        this->update();
+        //Check if there were any problems calculating the kinematics
+        return kinematics_status;
+    }
+    
+    void CartesianVelocityController::updateHook()
+    {
+        //Read out the ports
+        naxespos_port.Get(naxesposition);
+        cartvel_port.Get(cartvel);
+        
+        //Check if the jointpositions-port value as a correct size
+        if(nj==naxesposition.size()){
+            
+            //copy into KDL-type
+            for(unsigned int i=0;i<nj;i++)
+                jointpositions(i)=naxesposition[i];
+
+            //Calculate forward position kinematics
+            kinematics_status = (0<fksolver->JntToCart(jointpositions,cartpos));
+            //Only set result to port if it was calcuted correctly
+            if(kinematics_status)
+                cartpos_port.Set(cartpos);
+
+            //Calculate inverse velocity kinematics
+            kinematics_status = (0<iksolver->CartToJnt(jointpositions,cartvel,jointvelocities));
+            if(!kinematics_status)
+                SetToZero(jointvelocities);
+            
+            for(unsigned int i=0;i<nj;i++)
+                naxesvelocities[i]=jointvelocities(i);
+            
+            naxesvel_port.Set(naxesvelocities);
+        }
+        else
+            kinematics_status=false;
+    }
+}
+
+                                 
+                                 
+
---
+++ motion_control/cartesian/CartesianVelocityController.hpp
@@ -0,0 +1,88 @@
+// Copyright  (C)  2007  Ruben Smits <ruben dot smits at mech dot kuleuven dot be>
+
+// Author: Ruben Smits <ruben dot smits at mech dot kuleuven dot be>
+// URL: http://www.orocos.org/ocl
+
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+#ifndef __CARTESIAN_VELOCITY_CONTROLLER_HPP__
+#define __CARTESIAN_VELOCITY_CONTROLLER_HPP__
+
+#include <kdl/chain.hpp>
+#include <kdl/chainfksolver.hpp>
+#include <kdl/chainiksolver.hpp>
+#include <kdl/chainiksolvervel_pinv.hpp>
+#include <kdl/chainfksolverpos_recursive.hpp>
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Ports.hpp>
+
+#include <ocl/OCL.hpp>
+
+namespace OCL
+{
+    /**
+     * @file   CartesianVelocityController.hpp
+     * @author Ruben Smits
+     * @date   Mon Aug 20 11:24:08 2007
+     * 
+     * @brief  This component is the bridge between a hardware
+     * nAxesVelocityController and the Cartesian space control
+     * components. It calculates the forward position kinematics and
+     * the inverse velocity kinematics for a KDL::Chain, the kinematic
+     * algorithms are given by the user.
+     * 
+     */
+
+    class CartesianVelocityController : public RTT::TaskContext
+    {
+    public:
+        CartesianVelocityController(std::string name,const KDL::Chain& chain, 
+                                    KDL::ChainIkSolverVel* iksolver=NULL,
+                                    KDL::ChainFkSolverPos* fksolver=NULL);
+        ~CartesianVelocityController();
+        
+        virtual bool configureHook(){return true;};
+        virtual bool startHook();
+        virtual void updateHook();
+        virtual void stopHook(){};
+        virtual void cleanupHook(){};
+        
+    private:
+        const KDL::Chain& chain;
+        KDL::ChainFkSolverPos* fksolver;
+        KDL::ChainIkSolverVel* iksolver;
+        
+        unsigned int nj;
+        
+        KDL::JntArray jointpositions,jointvelocities;
+        std::vector<double> naxesposition,naxesvelocities;
+        
+        KDL::Twist    cartvel;
+        KDL::Frame    cartpos;
+        
+        RTT::DataPort<KDL::Frame> cartpos_port;
+        RTT::DataPort<KDL::Twist> cartvel_port;
+        RTT::DataPort<std::vector<double> > naxespos_port;
+        RTT::DataPort<std::vector<double> > naxesvel_port;
+        
+        bool kinematics_status,own_fk, own_ik;
+        
+    };
+}
+#endif
+    
+        
+        
