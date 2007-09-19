0461058dca3928bfc3b0647696ca855ed7fa9a39
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2007-09-19 15:07:39 +0000

    Strip CORBA code fromo deployer app.
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/ocl@28586 ce417995-dfc9-0310-95a0-acdaff106893

--- bin/deployer.cpp
+++ bin/deployer.cpp
@@ -3,10 +3,6 @@
 #include <taskbrowser/TaskBrowser.hpp>
 #include <deployment/DeploymentComponent.hpp>
 
-#ifdef OROPKG_CORBA
-#include <rtt/corba/ControlTaskServer.hpp>
-#endif
-
 int ORO_main(int argc, char** argv)
 {
     // deployer [DeployerName] [ -- TAO options ]
@@ -19,25 +15,7 @@ int ORO_main(int argc, char** argv)
     OCL::DeploymentComponent dc( name );
     OCL::TaskBrowser tb( &dc );
 
-    /**
-     * If CORBA is enabled, export the DeploymentComponent
-     * as CORBA server.
-     */
-#ifdef OROPKG_CORBA
-    using namespace RTT::Corba;
-    ControlTaskServer::InitOrb( argc, argv );
-
-    ControlTaskServer::Create( &dc );
-
-    ControlTaskServer::ThreadOrb();
-#endif
-
     tb.loop();
 
-#ifdef OROPKG_CORBA
-    ControlTaskServer::ShutdownOrb();
-
-    ControlTaskServer::DestroyOrb();
-#endif
     return 0;
 }
