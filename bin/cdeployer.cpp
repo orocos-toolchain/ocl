72bc23bab61f821c3f762784a62a83eb0f518476
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2007-10-30 13:08:38 +0000

    Add --start flag to cdeployer to kickStart an xml script.
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/ocl@28677 ce417995-dfc9-0310-95a0-acdaff106893

--- bin/cdeployer.cpp
+++ bin/cdeployer.cpp
@@ -2,17 +2,54 @@
 #include <rtt/RTT.hpp>
 #include <deployment/DeploymentComponent.hpp>
 #include <rtt/corba/ControlTaskServer.hpp>
+#include <iostream>
+
+using namespace std;
+
+void usage(const char* name)
+{
+    cout << "usage: " << name << " [DeployerName] [--start config-file.xml] [ -- TAO options]"<<endl;
+    cout << endl;
+}
 
 int ORO_main(int argc, char** argv)
 {
     // deployer [DeployerName] [ -- TAO options ]
-    char* name = "Deployer";
-    if ( argc > 1 ) {
-        std::string arg = argv[1];
-        if ( arg != "--" )
-            name = argv[1];
+    char* name = 0;
+    char* script = 0;
+    int i = 1;
+    while ( i < argc && std::string(argv[i]) != "--" ) {
+        std::string arg = argv[i];
+        if ( arg == "--help" ) {
+            usage(argv[0]);
+            return 0;
+        } else
+        if ( arg == "--start" ) {
+            ++i;
+            if ( i < argc ) {
+                script = argv[i];
+            } else {
+                cerr << "Please specify a filename after --start." <<endl;
+                usage(argv[0]);
+                return 0;
+            }
+        } else
+            if (name == 0)
+                name = argv[i];
+            else {
+                cerr << "Please specify only one name for the Deployer." <<endl;
+                usage(argv[0]);
+                return 0;
+            }
+        ++i;
     }
+
+    if (name == 0)
+        name = "Deployer";
     OCL::DeploymentComponent dc( name );
+
+    if (script)
+        dc.kickStart( script );
 
     /**
      * If CORBA is enabled, export the DeploymentComponent
