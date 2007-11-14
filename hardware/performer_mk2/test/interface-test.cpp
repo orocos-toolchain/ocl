41ef406fcf1a4b55ca1b57e8b6afab0d0aedf7cf
rsmits <rsmits@ce417995-dfc9-0310-95a0-acdaff106893>
2007-11-14 15:49:53 +0000

    adding tests for the performer, an xml-file for using the deployer and small testfile for the hardware interface
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/ocl@28706 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ hardware/performer_mk2/test/interface-test.cpp
@@ -0,0 +1,57 @@
+#include <rtt/RTT.hpp>
+#include <rtt/Activities.hpp>
+#include <rtt/TaskContext.hpp>
+#include <rtt/os/main.h>
+
+#include <ocl/TaskBrowser.hpp>
+#include <ocl/IOComponent.hpp>
+
+#include <ocl/dev/ComediDevice.hpp>
+#include <ocl/dev/ComediSubDeviceAOut.hpp>
+#include <ocl/dev/ComediSubDeviceDIn.hpp>
+#include <ocl/dev/ComediSubDeviceDOut.hpp>
+
+using namespace Orocos;
+
+int ORO_main(int arc, char* argv[])
+{
+  //Creating IOComponent and its activity
+  IOComponent io("IO");
+  PeriodicActivity io_act(ORO_SCHED_RT,RTT::OS::HighestPriority, 0.001);
+  io_act.run(io.engine());
+  
+  //Creating ComediDevices:
+  //ComediDevice* AOut = new ComediDevice(0);
+  ComediDevice* Encoder = new ComediDevice(2);
+  
+  //ComediSubDeviceAOut SubAOut(AOut,"AnalogOut",1);
+  ComediSubDeviceDOut SubDOut(Encoder,"DigitalOut",1);
+  //ComediSubDeviceDOut SubDOut(DInOut,"DigitalOut",1);
+  
+  //Adding Hardware interfaces to the IOComponent
+  io.addDigitalOutput("D0",&SubDOut,0);
+  io.addDigitalOutput("D1",&SubDOut,1);
+  io.addDigitalOutput("D2",&SubDOut,2);
+  io.addDigitalOutput("D3",&SubDOut,3);
+  io.addDigitalOutput("D4",&SubDOut,4);
+  io.addDigitalOutput("D5",&SubDOut,5);
+  io.addDigitalOutput("D6",&SubDOut,6);
+  io.addDigitalOutput("D7",&SubDOut,7);
+  io.addDigitalOutput("D8",&SubDOut,8);
+  io.addDigitalOutput("D9",&SubDOut,9);
+  
+  //Creating the TaskBrowser:
+  TaskBrowser tb(&io);
+  
+  tb.loop();
+  
+  //delete AOut;
+  //delete DInOut;
+  delete Encoder;
+  
+  return 0;
+}
+
+  
+
+
---
+++ hardware/performer_mk2/test/main.cpp
@@ -0,0 +1,45 @@
+//hardware interfaces
+
+#include <hardware/performer_mk2/PerformernAxesVelocityController.hpp>
+#include <hardware/kuka/EmergencyStop.hpp>
+
+
+//User interface
+#include <taskbrowser/TaskBrowser.hpp>
+
+//Reporting
+#include <reporting/FileReporting.hpp>
+
+#include <rtt/os/main.h>
+#include <rtt/PeriodicActivity.hpp>
+
+using namespace Orocos;
+using namespace RTT;
+using namespace std;
+
+/// main() function
+
+int ORO_main(int argc, char* argv[])
+{
+    
+  PerformerMK2nAxesVelocityController my_robot("Performer");
+  log(Info)<<"Robot Created"<<endlog();
+  
+  EmergencyStop _emergency(&my_robot);
+    
+  /// Creating Event Handlers
+  _emergency.addEvent(&my_robot,"driveOutOfRange");
+  
+  /// Link my_robot to Taskbrowser
+  TaskBrowser browser(&my_robot );
+  
+  /// Creating Tasks
+  PeriodicActivity robotTask(0,0.002, my_robot.engine() );  
+    
+  /// Start the console reader.
+  robotTask.start();
+  
+  browser.loop();
+  
+  return 0;
+}
