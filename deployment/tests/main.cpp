7b3d675eccc38d44ab1ced769f399910c7305fe1
Peter Soetens <peter@thesourceworks.com>
2009-12-03 14:39:00 +0100

    deployment: extend deployment test with properties and kickstart.

--- deployment/tests/main.cpp
+++ deployment/tests/main.cpp
@@ -4,6 +4,7 @@
 #include <rtt/InputPort.hpp>
 #include <rtt/os/main.h>
 #include <rtt/RTT.hpp>
+#include <vector>
 
 using namespace Orocos;
 
@@ -11,6 +12,7 @@ class MyTask
     : public RTT::TaskContext
 {
 public:
+    Property<std::vector<double> > limits;
     InputPort<double> p1;
     OutputPort<double> p2;
     OutputPort<double> p3;
@@ -18,15 +20,19 @@ public:
 
     MyTask(std::string n)
         : RTT::TaskContext(n),
+          limits("limits","desc", std::vector<double>(2) ),
           p1("p1"),
           p2("p2"),
           p3("p3"),
           a("a")
     {
+        this->properties()->addProperty(&limits);
         this->ports()->addPort( &p1, "");
         this->ports()->addPort( &p2, "");
         this->ports()->addPort( &p3, "");
         this->attributes()->addAttribute( &a );
+        limits.set()[0] = 10;
+        limits.set()[1] = 20;
     }
 };
 
@@ -39,7 +45,7 @@ int ORO_main(int, char**)
         DeploymentComponent dc;
         dc.addPeer( &t1 );
         dc.addPeer( &t2 );
-
+        dc.kickStart("deployment.cpf");
         TaskBrowser tb(&dc);
 
         tb.loop();
