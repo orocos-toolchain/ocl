15d370e096a40ad3f91549f10be101d5d64830bf
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2010-01-23 14:48:48 +0000

    deployment: extend deployment test with properties and kickstart.
    From: Peter Soetens <peter@thesourceworks.com>
    
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/ocl@31371 ce417995-dfc9-0310-95a0-acdaff106893

--- deployment/tests/main.cpp
+++ deployment/tests/main.cpp
@@ -4,6 +4,7 @@
 #include <rtt/DataPort.hpp>
 #include <rtt/os/main.h>
 #include <rtt/RTT.hpp>
+#include <vector>
 
 using namespace Orocos;
 
@@ -11,19 +12,24 @@ class MyTask
     : public TaskContext
 {
 public:
+  Property<std::vector<double> > limits;
     ReadDataPort<double> p1;
     WriteDataPort<double> p2;
     DataPort<double> p3;
 
     MyTask(std::string n)
         : TaskContext(n),
+	  limits("limits","desc", std::vector<double>(2) ),
           p1("p1"),
           p2("p2", 0.0),
           p3("p3", 0.0)
     {
-        this->ports()->addPort( &p1, "");
-        this->ports()->addPort( &p2, "");
-        this->ports()->addPort( &p3, "");
+      this->properties()->addProperty(&limits);
+      this->ports()->addPort( &p1, "");
+      this->ports()->addPort( &p2, "");
+      this->ports()->addPort( &p3, "");
+      limits.set()[0] = 10;
+      limits.set()[1] = 20;
     }
 };
 
@@ -36,7 +42,7 @@ int ORO_main(int, char**)
         DeploymentComponent dc;
         dc.addPeer( &t1 );
         dc.addPeer( &t2 );
-
+        dc.kickStart("deployment.cpf");
         TaskBrowser tb(&dc);
 
         tb.loop();
