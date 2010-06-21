dc8219de09480ef1ffd9793d0e8920262f5e26be
Peter Soetens <peter@thesourceworks.com>
2010-06-21 21:11:44 +0200

    taskbrowser: fix typos in tab completion and std out strings.

--- taskbrowser/TaskBrowser.cpp
+++ taskbrowser/TaskBrowser.cpp
@@ -268,7 +268,7 @@ namespace OCL
             tbcoms.push_back(".record");
             tbcoms.push_back(".end");
             tbcoms.push_back(".cancel");
-            tbcoms.push_back(".provides");
+            tbcoms.push_back(".provide");
             tbcoms.push_back(".services");
             tbcoms.push_back(".typekits");
             tbcoms.push_back(".types");
@@ -1093,7 +1093,7 @@ namespace OCL
         }
         if (instr == "services") {
             vector<string> names = PluginLoader::Instance()->listServices();
-            cout << "Available Plugins: ";
+            cout << "Available Services: ";
             for (std::vector<std::string>::iterator it = names.begin(); it != names.end(); ++it) {
                 cout << " " << *it;
             }
