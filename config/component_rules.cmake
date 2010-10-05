6a32d00e0cd87cf99c11ca22ca5c62fe421494ec
psoetens <psoetens@ce417995-dfc9-0310-95a0-acdaff106893>
2010-10-05 11:56:01 +0000

    cmake: install components in lib/orocos or lib
    This is a copy of the 2.0 version of the component_rules file.
    
    Signed-off-by: Peter Soetens <peter@thesourceworks.com>
    
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/ocl@33088 ce417995-dfc9-0310-95a0-acdaff106893

--- config/component_rules.cmake
+++ config/component_rules.cmake
@@ -1,6 +1,38 @@
 #
 # Include and link against required stuff
 #
+#From: http://www.cmake.org/Wiki/CMakeMacroParseArguments
+MACRO(PARSE_ARGUMENTS prefix arg_names option_names)
+  SET(DEFAULT_ARGS)
+  FOREACH(arg_name ${arg_names})  
+    SET(${prefix}_${arg_name})
+  ENDFOREACH(arg_name)
+  FOREACH(option ${option_names})
+    SET(${prefix}_${option} FALSE)
+  ENDFOREACH(option)
+
+  SET(current_arg_name DEFAULT_ARGS)
+  SET(current_arg_list)
+  FOREACH(arg ${ARGN})
+    SET(larg_names ${arg_names})
+    LIST(FIND larg_names "${arg}" is_arg_name)     
+    IF (is_arg_name GREATER -1)
+      SET(${prefix}_${current_arg_name} ${current_arg_list})
+      SET(current_arg_name ${arg})
+      SET(current_arg_list)
+    ELSE (is_arg_name GREATER -1)
+      SET(loption_names ${option_names})
+      LIST(FIND loption_names "${arg}" is_option)            
+      IF (is_option GREATER -1)
+         SET(${prefix}_${arg} TRUE)
+      ELSE (is_option GREATER -1)
+         SET(current_arg_list ${current_arg_list} ${arg})
+      ENDIF (is_option GREATER -1)
+    ENDIF (is_arg_name GREATER -1)
+  ENDFOREACH(arg)
+  SET(${prefix}_${current_arg_name} ${current_arg_list})
+ENDMACRO(PARSE_ARGUMENTS)
+
 #
 # Components supply header files which should be included when 
 # using these components. Each component should use this macro
@@ -8,14 +40,25 @@
 #
 # Usage: GLOBAL_ADD_INCLUDE( RELATIVE_LOCATION hpp1, hpp2 ...)
 MACRO( GLOBAL_ADD_INCLUDE COMPONENT_LOCATION )
-  INSTALL(FILES ${ARGN} DESTINATION include/${COMPONENT_LOCATION} )
+  INSTALL( FILES ${ARGN} DESTINATION include/${COMPONENT_LOCATION} )
 ENDMACRO( GLOBAL_ADD_INCLUDE COMPONENT_LOCATION )
 
 # Link a component library with an external library (qt3, gl, readline,....)
 # Usage: COMPONENT_ADD_LIBS( orocos-taskbrowser readline ncurses )
+#
+# Explicitly deal with library lists of the form "optimized;xxx;debug;xxx-d"
+# These lists can be chained e.g. "optimized;xxx;debug;xxx-d;optimized;yyy;debug;yyy-d"
+#
 MACRO( COMPONENT_ADD_LIBS COMPONENT_NAME  )
     TARGET_LINK_LIBRARIES( ${COMPONENT_NAME}-${OROCOS_TARGET} ${ARGN} )
 ENDMACRO( COMPONENT_ADD_LIBS COMPONENT_NAME )
+
+# Link a component library with a list of external libraries (qt3, gl, readline,....)
+# Usage: COMPONENT_ADD_LIBS( orocos-taskbrowser ${Boost_FILESYSTEM_LIBRARIES} )
+MACRO( COMPONENT_ADD_LIB_LIST COMPONENT_NAME  )
+  MESSAGE("ADD_LIB_LIST '${ARGN}'")
+  TARGET_LINK_LIBRARIES( ${COMPONENT_NAME}-${OROCOS_TARGET} ${ARGN} )
+ENDMACRO( COMPONENT_ADD_LIB_LIST COMPONENT_NAME )
 
 # Link a component library with another component library
 # Usage: COMPONENT_ADD_DEPS( orocos-taskbrowser orocos-reporting )
@@ -31,7 +74,74 @@ MACRO( PROGRAM_ADD_DEPS PROGRAM_NAME )
   foreach( lib ${ARGN} )
     TARGET_LINK_LIBRARIES( ${PROGRAM_NAME} ${lib}-${OROCOS_TARGET} )
   endforeach( lib ${ARGN} )
+  SET_TARGET_PROPERTIES( ${PROGRAM_NAME} PROPERTIES 
+    INSTALL_RPATH_USE_LINK_PATH 1
+    INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/lib/orocos")
 ENDMACRO( PROGRAM_ADD_DEPS PROGRAM_NAME )
+
+# Link a program with an external library (qt3, gl, readline,....)
+# Usage: PROGRAM_ADD_LIBS( taskbrowser-test readline ncurses )
+MACRO( PROGRAM_ADD_LIBS PROGRAM_NAME )
+  foreach( lib ${ARGN} )
+    TARGET_LINK_LIBRARIES( ${PROGRAM_NAME} ${lib} )
+  endforeach( lib ${ARGN} )
+ENDMACRO( PROGRAM_ADD_LIBS PROGRAM_NAME )
+
+
+#
+# Components should add themselves by calling 'CREATE_COMPONENT'
+# instead of 'ADD_LIBRARY' in CMakeLists.txt. This macro will _NOT_
+# install this library, as opposed to GLOBAL_ADD_COMPONENT which does.
+# This macro is meant for test libraries built as part of OCL.
+#
+#
+# Usage: CREATE_COMPONENT( COMPONENT_NAME src1 src2 src3 )
+#
+MACRO( CREATE_COMPONENT COMPONENT_NAME )
+
+  SET( LIB_NAME "${COMPONENT_NAME}-${OROCOS_TARGET}")
+
+  IF(GLOBAL_LIBRARY)
+    MESSAGE( ERROR "BROKEN: Adding ${COMPONENT_NAME} to global sources:[ ${GLOBAL_LIBRARY_SRCS} ]" )
+    SET (GLOBAL_LIBRARY_SRCS "${GLOBAL_LIBRARY_SRCS} ${ARGN}" )
+  ENDIF(GLOBAL_LIBRARY)
+
+  IF(LOCAL_LIBRARY)
+    IF (OROCOS_RTT_1.4)
+      MESSAGE( "Building Shared library for ${COMPONENT_NAME}" )
+      ADD_LIBRARY( ${LIB_NAME} SHARED ${ARGN} )
+      SET_TARGET_PROPERTIES( ${LIB_NAME} PROPERTIES
+		DEFINE_SYMBOL OCL_DLL_EXPORT
+		VERSION ${OCL_VERSION}
+		SOVERSION ${OCL_VERSION_MAJOR}.${OCL_VERSION_MINOR}
+		)
+      foreach(lib ${OROCOS-RTT_LIBRARIES})
+		TARGET_LINK_LIBRARIES( ${LIB_NAME} ${lib} )
+      endforeach(lib in ${OROCOS-RTT_LIBRARIES})
+
+    ELSE (OROCOS_RTT_1.4)
+
+      IF (OROCOS_RTT_1.2)
+		MESSAGE( "Building Shared library for ${COMPONENT_NAME}" )
+		ADD_LIBRARY( ${LIB_NAME} SHARED ${ARGN} )
+		SET_TARGET_PROPERTIES( ${LIB_NAME} PROPERTIES
+		  DEFINE_SYMBOL OCL_DLL_EXPORT
+		  VERSION ${OCL_VERSION}
+		  SOVERSION ${OCL_VERSION_MAJOR}.${OCL_VERSION_MINOR}
+		  )
+		TARGET_LINK_LIBRARIES( ${LIB_NAME} orocos-rtt )
+      ELSE (OROCOS_RTT_1.2)
+		MESSAGE( "Building Static library for ${COMPONENT_NAME}" )
+		ADD_LIBRARY( ${LIB_NAME} STATIC ${ARGN} )
+      ENDIF (OROCOS_RTT_1.2)
+
+    ENDIF (OROCOS_RTT_1.4)
+
+    LINK_DIRECTORIES( ${CMAKE_CURRENT_BINARY_DIR} )
+
+  ENDIF(LOCAL_LIBRARY)
+
+ENDMACRO( CREATE_COMPONENT COMPONENT_NAME )
 
 
 #
@@ -42,62 +152,44 @@ ENDMACRO( PROGRAM_ADD_DEPS PROGRAM_NAME )
 # and lets us add various things to all components in just one place.
 #
 #
-# Usage: GLOBAL_ADD_COMPONENT( COMPONENT_NAME src1 src2 src3 )
+# Usage: GLOBAL_ADD_COMPONENT( COMPONENT_NAME src1 src2 src3 [INSTALL lib/orocos] )
 #
-
-MACRO( GLOBAL_ADD_COMPONENT COMPONENT_NAME )
+MACRO( GLOBAL_ADD_COMPONENT )
   
-#  IF(STANDALONE_COMPONENTS)
-#     MESSAGE( "BROKEN: Building Stand-alone component ${COMPONENT_NAME}" )
-#     ADD_EXECUTABLE( ${COMPONENT_NAME} ${ARGN} )
-#     #INSTALL_TARGETS( /bin ${COMPONENT_NAME} )
-#     TARGET_LINK_LIBRARIES( ${COMPONENT_NAME} ${OROCOS_RTT_LIBS} )
-#  ENDIF(STANDALONE_COMPONENTS)
-
+  PARSE_ARGUMENTS(ADD_COMPONENT
+    "INSTALL"
+    ""
+    ${ARGN}
+    )
+  list(GET ADD_COMPONENT_DEFAULT_ARGS 0 COMPONENT_NAME)
+  list(REMOVE_AT ADD_COMPONENT_DEFAULT_ARGS 0)
+  SET( SOURCES ${ADD_COMPONENT_DEFAULT_ARGS} )
   SET( LIB_NAME "${COMPONENT_NAME}-${OROCOS_TARGET}")
+  if ( ADD_COMPONENT_INSTALL )
+    set(AC_INSTALL_DIR ${ADD_COMPONENT_INSTALL})
+  else()
+    set(AC_INSTALL_DIR lib/orocos)
+  endif()
   
-  IF(GLOBAL_LIBRARY)
-     MESSAGE( ERROR "BROKEN: Adding ${COMPONENT_NAME} to global sources:[ ${GLOBAL_LIBRARY_SRCS} ]" )
-     SET (GLOBAL_LIBRARY_SRCS "${GLOBAL_LIBRARY_SRCS} ${ARGN}" )
-  ENDIF(GLOBAL_LIBRARY)
+  MESSAGE( "Building Shared library for ${COMPONENT_NAME}" )
+  ADD_LIBRARY( ${LIB_NAME} SHARED ${SOURCES} )
+  SET_TARGET_PROPERTIES( ${LIB_NAME} PROPERTIES 
+    DEFINE_SYMBOL "OCL_DLL_EXPORT"
+    VERSION ${OCL_VERSION}
+    SOVERSION ${OCL_VERSION_MAJOR}.${OCL_VERSION_MINOR}
+    INSTALL_RPATH_USE_LINK_PATH 1
+    )
+  TARGET_LINK_LIBRARIES( ${LIB_NAME} ${OROCOS-RTT_LIBRARIES} )
 
-  IF(LOCAL_LIBRARY)
-    IF (OROCOS_RTT_1.4)
-      MESSAGE( "Building Shared library for ${COMPONENT_NAME}" )
-      ADD_LIBRARY( ${LIB_NAME} SHARED ${ARGN} )
-      SET_TARGET_PROPERTIES( ${LIB_NAME} PROPERTIES 
-	DEFINE_SYMBOL "OCL_DLL_EXPORT"
-	VERSION ${OCL_VERSION}
-	SOVERSION ${OCL_VERSION_MAJOR}.${OCL_VERSION_MINOR}
-	)
- 	TARGET_LINK_LIBRARIES( ${LIB_NAME} ${OROCOS_RTT_LIBS} )
 
-    ELSE (OROCOS_RTT_1.4)
-      IF (OROCOS_RTT_1.2)
-	MESSAGE( "Building Shared library for ${COMPONENT_NAME}" )
-	ADD_LIBRARY( ${LIB_NAME} SHARED ${ARGN} )
-	SET_TARGET_PROPERTIES( ${LIB_NAME} PROPERTIES 
-	  DEFINE_SYMBOL OCL_DLL_EXPORT 
-	  VERSION ${OCL_VERSION}
-	  SOVERSION ${OCL_VERSION_MAJOR}.${OCL_VERSION_MINOR}
-	  )
-	TARGET_LINK_LIBRARIES( ${LIB_NAME} orocos-rtt )
-      ELSE (OROCOS_RTT_1.2)
-	MESSAGE( "Building Static library for ${COMPONENT_NAME}" )
-	ADD_LIBRARY( ${LIB_NAME} STATIC ${ARGN} )
-      ENDIF (OROCOS_RTT_1.2)
-    ENDIF (OROCOS_RTT_1.4)
+  INSTALL(TARGETS ${LIB_NAME} LIBRARY DESTINATION ${AC_INSTALL_DIR} ARCHIVE DESTINATION lib RUNTIME DESTINATION bin)
+  #The later a component is added, the earlier it apears in the -l list.
+  SET (ENV{SELECTED_LIBS} "-l${LIB_NAME} $ENV{SELECTED_LIBS} ")
+  #This is an ugly work around
+  #FILE(APPEND ${PROJ_BINARY_DIR}/bibs.txt "-l${LIB_NAME} ")
 
-    INSTALL( TARGETS ${LIB_NAME} ARCHIVE DESTINATION lib LIBRARY DESTINATION lib RUNTIME DESTINATION bin )
-    #The later a component is added, the earlier it apears in the -l list.
-    SET (ENV{SELECTED_LIBS} "-l${LIB_NAME} $ENV{SELECTED_LIBS} ")
-    #This is an ugly work around
-    #FILE(APPEND ${PROJ_BINARY_DIR}/bibs.txt "-l${LIB_NAME} ")
-
-    LINK_DIRECTORIES( ${CMAKE_CURRENT_BINARY_DIR} )
-    SET (ENV{SELECTED_DIRS} "$ENV{SELECTED_DIRS} ${CMAKE_CURRENT_SOURCE_DIR}")
-    
-  ENDIF(LOCAL_LIBRARY)
+  LINK_DIRECTORIES( ${CMAKE_CURRENT_BINARY_DIR} )
+  SET (ENV{SELECTED_DIRS} "$ENV{SELECTED_DIRS} ${CMAKE_CURRENT_SOURCE_DIR}")
 
 ENDMACRO( GLOBAL_ADD_COMPONENT COMPONENT_NAME )
 
@@ -120,8 +212,6 @@ MACRO( GLOBAL_ADD_TEST TEST_NAME )
   IF(BUILD_TESTS)
 	ADD_EXECUTABLE( ${TEST_NAME} ${ARGN} )
 	ADD_TEST( ${TEST_NAME} ${TEST_NAME} )
-	#INSTALL_TARGETS( /bin/${TEST_NAME} ${TEST_NAME} )
-
 	MESSAGE("Planning to build test: ${TEST_NAME}" )
   ELSE(BUILD_TESTS)
 	MESSAGE("Not building test: ${TEST_NAME}" )
