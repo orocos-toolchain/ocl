f594739c8d0b71a50bce2401c6f90b62eec8a1fb
Markus Klotzbuecher <markus.klotzbuecher@mech.kuleuven.be>
2010-10-07 14:59:17 +0200

    lua: add license headers

--- lua/lua-repl.c
+++ lua/lua-repl.c
@@ -1,4 +1,9 @@
 /*
+ * Copied from the Lua sources. To remain diff'able unused parts are
+ * disable with #ifdefs
+ */
+
+/*
 ** $Id: lua.c,v 1.160.1.2 2007/12/28 15:32:23 roberto Exp $
 ** Lua stand-alone interpreter
 ** See Copyright Notice in lua.h
--- lua/lua-repl.h
+++ lua/lua-repl.h
@@ -1,4 +1,8 @@
 /*
+ * Copied from the Lua sources.
+ */
+
+/*
 ** $Id: lua.h,v 1.218.1.5 2008/08/06 13:30:12 roberto Exp $
 ** Lua - An Extensible Extension Language
 ** Lua.org, PUC-Rio, Brazil (http://www.lua.org)
