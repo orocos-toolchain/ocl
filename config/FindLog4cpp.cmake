87ea0cc148f09f7f7298fa5f813368dfbc2e43c3
Stephen Roderick <kiwi.net@mac.com>
2011-03-06 16:02:34 -0500

    cmake: Fix name of Orocos' log4cpp library that is searched for on macosx
    Signed-off-by: Stephen Roderick <kiwi.net@mac.com>

--- config/FindLog4cpp.cmake
+++ config/FindLog4cpp.cmake
@@ -34,8 +34,20 @@ find_library(LOG4CPP_LIBRARY NAMES log4cpp ${LOG4CPP_LIBRARY_HINTS})
 
 set(LOG4CPP_VERSION 6.0.0)
 message("Log4cpp version to look for: ${LOG4CPP_VERSION} (hard-coded in FindLog4cpp.cmake).")
-if (LOG4CPP_LIBRARY AND NOT EXISTS ${LOG4CPP_LIBRARY}.${LOG4CPP_VERSION} )
-  message("File  ${LOG4CPP_LIBRARY}.${LOG4CPP_VERSION} does not exist. Are you using the Orocos' version of log4cpp ?")
+
+# Determine name of Orocos' version of the log4cpp library, and see if it exists
+# Linux has    liblog4cpp.so.6.0.0
+# Mac OS X has liblog4cpp.6.0.0.dylib
+IF (APPLE)
+  GET_FILENAME_COMPONENT(LOG4CPP_OROCOS_LIB_PATH "${LOG4CPP_LIBRARY}" PATH)
+  GET_FILENAME_COMPONENT(LOG4CPP_OROCOS_LIB_NAME_WE "${LOG4CPP_LIBRARY}" NAME_WE)
+  GET_FILENAME_COMPONENT(LOG4CPP_OROCOS_LIB_EXT "${LOG4CPP_LIBRARY}" EXT)
+  SET(LOG4CPP_OROCOS_LIB "${LOG4CPP_OROCOS_LIB_PATH}/${LOG4CPP_OROCOS_LIB_NAME_WE}.${LOG4CPP_VERSION}${LOG4CPP_OROCOS_LIB_EXT}")
+ELSE (APPLE)
+  SET(LOG4CPP_OROCOS_LIB "${LOG4CPP_LIBRARY}.${LOG4CPP_VERSION}")
+ENDIF (APPLE)
+if (LOG4CPP_LIBRARY AND NOT EXISTS ${LOG4CPP_OROCOS_LIB} )
+  message("File  ${LOG4CPP_OROCOS_LIB} does not exist. Are you using the Orocos' version of log4cpp ?")
   message("  Use CMAKE_PREFIX_PATH to point to the install directory of the Orocos maintained log4cpp library.")
 endif()
 
