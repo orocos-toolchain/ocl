dba06c0df3262457a18fa7e43d8410918a279758
Peter Soetens <peter@thesourceworks.com>
2011-05-17 16:29:07 +0200

    logging: implement socket appender for using ChainSaw
    ChainSaw needs to be running first.
    
    Signed-off-by: Peter Soetens <peter@thesourceworks.com>

---
+++ logging/Log4cxxAppender.hpp
@@ -0,0 +1,81 @@
+#ifndef	socketAppender_HPP
+#define	socketAppender_HPP 1
+
+#include <rtt/TaskContext.hpp>
+#include <rtt/Property.hpp>
+#include <rtt/InputPort.hpp>
+#include "LoggingEvent.hpp"
+#include <log4cxx/logger.h>
+#include <log4cxx/helpers/pool.h>
+#include <log4cxx/basicconfigurator.h>
+#include <log4cxx/net/socketappender.h>
+#include <log4cxx/simplelayout.h>
+
+#if 0
+int main() {
+   log4cxx::Log4cxxAppender * socketAppender = new
+log4cxx::Log4cxxAppender(log4cxx::LayoutPtr(new log4cxx::SimpleLayout()),
+"logfile", false);
+
+   log4cxx::helpers::Pool p;
+   socketAppender->activateOptions(p);
+
+   log4cxx::BasicConfigurator::configure(log4cxx::AppenderPtr(socketAppender));
+   log4cxx::Logger::getRootLogger()->setLevel(log4cxx::Level::getDebug());
+   log4cxx::LoggerPtr logger = log4cxx::Logger::getLogger("logger");
+
+   LOG4CXX_INFO(logger,"Created Log4cxxAppender appender");
+
+   return 0;
+}
+#endif
+
+namespace OCL {
+namespace logging {
+
+class Log4cxxAppender
+    : public RTT::TaskContext
+{
+public:
+	Log4cxxAppender(std::string name);
+	virtual ~Log4cxxAppender();
+protected:
+    virtual bool configureHook();
+	virtual void updateHook();
+	virtual void cleanupHook();
+
+	log4cxx::helpers::Pool p;
+	log4cxx::net::SocketAppender * socketAppender;
+	log4cxx::helpers::InetAddressPtr address;
+
+    /// Port we receive logging events on
+    /// Initially unconnected. The logging service connects appenders.
+    RTT::InputPort<OCL::logging::LoggingEvent> log_port;
+
+	   /// Name of host to append to
+    std::string      hostname_prop;
+
+    /// The port where the logging server runs.
+    int port_prop;
+    /**
+     * Property to set maximum number of log events to pop per cycle
+     */
+    int              maxEventsPerCycle_prop;
+
+    /**
+     * Maximum number of log events to pop per cycle
+     *
+     * Defaults to 1.
+     *
+     * A value of 0 indicates to not limit the number of events per cycle.
+     * With enough event production, this could lead to thread
+     * starvation!
+     */
+    int                           maxEventsPerCycle;
+};
+
+// namespaces
+}
+}
+
+#endif
