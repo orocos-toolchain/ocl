2627e975739708a85c24168da23ebb93c8f6a5cd
Markus Klotzbuecher <markus.klotzbuecher@mech.kuleuven.be>
2011-05-20 15:42:33 +0200

    Lua, rttlib: fix in fromtab.
    Account for the fact that array indices start from zero in RTT and one
    in Lua.

--- lua/modules/rttlib.lua
+++ lua/modules/rttlib.lua
@@ -193,7 +193,9 @@ function varfromtab(var, tab)
    if type(tab) ~= 'table' then error("arg 2 is not a table") end
 
    for k,v in pairs(tab) do
-      memdsb = var:getMemberRaw(k)
+      if type(k) == 'number' then memdsb = var:getMemberRaw(k-1)
+      else memdsb = var:getMemberRaw(k) end
+
       if memdsb == nil then error("no member " .. k) end
 
       if rtt.Variable.isbasic(memdsb) then
