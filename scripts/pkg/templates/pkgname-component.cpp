1318fcee2324fcc41cb5dce694122fc63a8d85d8
Steven Bellens <steven.bellens@mech.kuleuven.be>
2012-03-07 14:21:51 +0100

    moving component hook implementations to .cpp file
    Signed-off-by: Steven Bellens <steven.bellens@mech.kuleuven.be>

--- scripts/pkg/templates/pkgname-component.cpp
+++ scripts/pkg/templates/pkgname-component.cpp
@@ -1,10 +1,37 @@
 #include "@pkgname@-component.hpp"
 #include <rtt/Component.hpp>
+#include <iostream>
+
+@Pkgname@::@Pkgname@(std::string const& name) : TaskContext(name){
+  std::cout << "@Pkgname@ constructed !" <<std::endl;
+}
+
+bool @Pkgname@::configureHook(){
+  std::cout << "@Pkgname@ configured !" <<std::endl;
+  return true;
+}
+
+bool @Pkgname@::startHook(){
+  std::cout << "@Pkgname@ started !" <<std::endl;
+  return true;
+}
+
+void @Pkgname@::updateHook(){
+  std::cout << "@Pkgname@ executes updateHook !" <<std::endl;
+}
+
+void @Pkgname@::stopHook() {
+  std::cout << "@Pkgname@ executes stopping !" <<std::endl;
+}
+
+void @Pkgname@::cleanupHook() {
+  std::cout << "@Pkgname@ cleaning up !" <<std::endl;
+}
 
 /*
  * Using this macro, only one component may live
  * in one library *and* you may *not* link this library
- * with another component library. Use 
+ * with another component library. Use
  * ORO_CREATE_COMPONENT_TYPE()
  * ORO_LIST_COMPONENT_TYPE(@Pkgname@)
  * In case you want to link with another library that
