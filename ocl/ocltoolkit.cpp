92bf409335622002b6b44d526888a3f9e4329e52
Peter Soetens <peter@thesourceworks.com>
2012-10-11 00:00:48 +0200

    ocl: drop support for bools and add doubles again
    bools segfaults...
    
    Signed-off-by: Peter Soetens <peter@thesourceworks.com>

--- ocl/ocltoolkit.cpp
+++ ocl/ocltoolkit.cpp
@@ -36,9 +36,10 @@ namespace OCL
         bool loadTypes() {
             RTT::types::TypeInfoRepository::Instance()->addType( new types::SequenceTypeInfo<vector<std::string> >("strings") );
 
-            RTT::types::TypeInfoRepository::Instance()->addType( new types::SequenceTypeInfo<vector<bool> >("bools") );
+            // segfaults when reading out an element of this type:
+            //RTT::types::TypeInfoRepository::Instance()->addType( new types::SequenceTypeInfo<vector<bool> >("bools") );
 
-            //RTT::types::TypeInfoRepository::Instance()->addType( new types::SequenceTypeInfo<vector<double> >("doubles") );
+            RTT::types::TypeInfoRepository::Instance()->addType( new types::SequenceTypeInfo<vector<double> >("doubles") );
 
             RTT::types::TypeInfoRepository::Instance()->addType( new types::SequenceTypeInfo<vector<int> >("ints") );
 
