080fe664f8ef9a7e73d46f81e054c25a4b1cd634
Johannes Meyer <johannes@intermodalics.eu>
2015-12-18 19:15:23 +0100

    config: renamed log4cpp library to orocos-log4cpp (see orocos-toolchain/log4cpp#4)
    Signed-off-by: Johannes Meyer <johannes@intermodalics.eu>

--- config/FindLog4cpp.cmake
+++ config/FindLog4cpp.cmake
@@ -27,33 +27,14 @@ ENDIF ()
 
 # If LOG4CPP_ROOT is available, set up our hints
 IF (LOG4CPP_ROOT)
-    SET (LOG4CPP_INCLUDE_HINTS HINTS "${LOG4CPP_ROOT}/include" "${LOG4CPP_ROOT}")
+    SET (LOG4CPP_INCLUDE_HINTS HINTS "${LOG4CPP_ROOT}/include" PATH_SUFFIXES orocos)
     SET (LOG4CPP_LIBRARY_HINTS HINTS "${LOG4CPP_ROOT}/lib")
 ENDIF ()
 
 # Find headers and libraries
 find_path(LOG4CPP_INCLUDE_DIR NAMES log4cpp/Category.hh ${LOG4CPP_INCLUDE_HINTS})
-find_library(LOG4CPP_LIBRARY NAMES log4cpp ${LOG4CPP_LIBRARY_HINTS})
-find_library(LOG4CPPD_LIBRARY NAMES log4cpp${CMAKE_DEBUG_POSTFIX} ${LOG4CPP_LIBRARY_HINTS})
-
-set(LOG4CPP_VERSION 6.0.0)
-message("Log4cpp version to look for: ${LOG4CPP_VERSION} (hard-coded in FindLog4cpp.cmake).")
-
-# Determine name of Orocos' version of the log4cpp library, and see if it exists
-# Linux has    liblog4cpp.so.6.0.0
-# Mac OS X has liblog4cpp.6.0.0.dylib
-IF (APPLE)
-  GET_FILENAME_COMPONENT(LOG4CPP_OROCOS_LIB_PATH "${LOG4CPP_LIBRARY}" PATH)
-  GET_FILENAME_COMPONENT(LOG4CPP_OROCOS_LIB_NAME_WE "${LOG4CPP_LIBRARY}" NAME_WE)
-  GET_FILENAME_COMPONENT(LOG4CPP_OROCOS_LIB_EXT "${LOG4CPP_LIBRARY}" EXT)
-  SET(LOG4CPP_OROCOS_LIB "${LOG4CPP_OROCOS_LIB_PATH}/${LOG4CPP_OROCOS_LIB_NAME_WE}.${LOG4CPP_VERSION}${LOG4CPP_OROCOS_LIB_EXT}")
-ELSE (APPLE)
-  SET(LOG4CPP_OROCOS_LIB "${LOG4CPP_LIBRARY}.${LOG4CPP_VERSION}")
-ENDIF (APPLE)
-if (LOG4CPP_LIBRARY AND NOT EXISTS ${LOG4CPP_OROCOS_LIB} )
-  message("File  ${LOG4CPP_OROCOS_LIB} does not exist. Are you using the Orocos' version of log4cpp ?")
-  message("  Use CMAKE_PREFIX_PATH to point to the install directory of the Orocos maintained log4cpp library.")
-endif()
+find_library(LOG4CPP_LIBRARY NAMES orocos-log4cpp ${LOG4CPP_LIBRARY_HINTS})
+find_library(LOG4CPPD_LIBRARY NAMES orocos-log4cpp${CMAKE_DEBUG_POSTFIX} ${LOG4CPP_LIBRARY_HINTS})
 
 # Set LOG4CPP_FOUND honoring the QUIET and REQUIRED arguments
 find_package_handle_standard_args(LOG4CPP DEFAULT_MSG LOG4CPP_LIBRARY LOG4CPP_INCLUDE_DIR)
